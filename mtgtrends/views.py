532ed41ee3d5bf5e57a69f460540296da4e29ef9
Upauper63 <syunufkin@gmail.com>
2021-07-17 17:48:07 +0900

    プロトタイプ完成

--- mtgtrends/views.py
+++ mtgtrends/views.py
@@ -1,3 +1,48 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.template.response import TemplateResponse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+import requests, json, pandas as pd, io, matplotlib, matplotlib.pyplot as plt, base64
+matplotlib.use('Agg')
 
-# Create your views here.
+def index(request):
+    params = request.GET.copy()
+
+    # 語句検索
+    search_word = request.GET.get('name')
+    if search_word:
+        res = requests.get('http://127.0.0.1:8000/api/items/?search=' + search_word)
+    else:
+        res = requests.get('http://127.0.0.1:8000/api/items/')
+        search_word = ''
+
+    items = json.loads(res.text)
+
+    # ページネータ処理
+    if 'page' in params:
+        page = params['page']
+        del params['page']
+    else:
+        page = 1
+    search_params = params.urlencode()
+    paginator = Paginator(items, 50)
+    try:
+        items = paginator.page(page)
+    except (EmptyPage, PageNotAnInteger):
+        items = paginator.page(1)
+
+    return TemplateResponse(request, 'mtgtrends/index.html', {'items': items, 'search_params': search_params, 'search_word': search_word})
+
+
+def detail(request, item_id):
+    res = requests.get('http://127.0.0.1:8000/api/item/?id=' + str(item_id))
+    item = json.loads(res.text)
+    df = pd.DataFrame(item[0]['trends'], columns=['Date', 'Price'])
+    df.plot(x="Date", y="Price", ylabel='Price', rot=30, marker="o", legend=None)
+    buf = io.BytesIO()
+    plt.savefig(buf, format='png', bbox_inches='tight')
+    svg = buf.getvalue()
+    graph = base64.b64encode(svg).decode('utf-8')
+    buf.close()
+    plt.cla()
+    return TemplateResponse(request, 'mtgtrends/detail.html', {'graph': graph, 'name': item[0]['name']})
