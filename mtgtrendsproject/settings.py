f8943ff8d8ee090a2ecde990abad27a79f620b91
Upauper63 <syunufkin@gmail.com>
2021-07-17 21:20:42 +0900

    微修正

--- mtgtrendsproject/settings.py
+++ mtgtrendsproject/settings.py
@@ -117,4 +117,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-django_heroku.settings(locals())
+django_heroku.settings(locals(), staticfiles=False)
