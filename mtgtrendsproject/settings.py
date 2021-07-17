aa4a9981c564eaf8a8b0627f27a5b7bd13ad92c7
Upauper63 <syunufkin@gmail.com>
2021-07-17 20:42:07 +0900

    微修正

--- mtgtrendsproject/settings.py
+++ mtgtrendsproject/settings.py
@@ -107,7 +107,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
