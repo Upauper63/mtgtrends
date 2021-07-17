06a5116f58000877080f85e95bf5d4377e7b081c
Upauper63 <syunufkin@gmail.com>
2021-07-17 19:36:48 +0900

    プロトタイプfix

--- mtgtrendsproject/settings.py
+++ mtgtrendsproject/settings.py
@@ -114,6 +114,6 @@ STATICFILES_DIRS = (
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 django_heroku.settings(locals())
